019eca2e36496ff8d874ecbd8ce222dc7b4c428e
Maxim Akimov <akimovmax@mail.ru>
2018-06-11 01:16:35 +0300

    Вставляет сгенерированные данные на страницу

--- js/pictures.js
+++ js/pictures.js
@@ -1,11 +1,17 @@
 'use strict';
 
+// Функция получения рандомных чисел из диапазона
+var getRandomNumber = function (min, max) {
+  return Math.floor((Math.random() * (max - min + 1) + min));
+};
+
+// Функция получения случайного значения из массива
+var getRandomArrayValue = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
 // Функция генерации данных
 var generateData = function () {
-
-  // -------
-  // 1. Константы
-  // -------
 
   // Максимальное значение номера фотографии
   var MAX_PHOTO_NUMBER = 25;
@@ -39,26 +45,8 @@ var generateData = function () {
   // Количество фраз в генерируемом комментарии
   var PHRASE_NUMBERS = [1, 2];
 
-  // -------
-  // 2. Переменные
-  // -------
-
   // Массив данных о фотографиях
   var photosData = [];
-
-  // -------
-  // 3. Функции
-  // -------
-
-  // Функция получения рандомных чисел из диапазона
-  var getRandomNumber = function (min, max) {
-    return Math.floor((Math.random() * (max - min + 1) + min));
-  };
-
-  // Функция получения случайного значения из массива
-  var getRandomArrayValue = function (array) {
-    return array[Math.floor(Math.random() * array.length)];
-  };
 
   // Функция получения пути к фотографии
   var getUrl = function (numeral) {
@@ -100,7 +88,90 @@ var generateData = function () {
     };
   }
 
-  console.log(photosData);
+  return photosData;
 };
 
-generateData();
+// Переменная с массивом данных из функции generateData()
+var data = generateData();
+
+// Блок шаблона
+var photoTemplate = document.querySelector('#picture')
+.content
+.querySelector('.picture__link');
+
+// Фрагмент
+var fragment = document.createDocumentFragment();
+
+// Пустой контейнер фотографий
+var photosList = document.querySelector('.pictures');
+
+// Функция создания DOM-элемента на основе шаблона
+var createPhoto = function (array) {
+  // Клон тега <a> из шаблона
+  var photo = photoTemplate.cloneNode(true);
+
+  // Наполнение тега <a> контентом
+  // 1. Содержимое счетчика лайков
+  photo.querySelector('.picture__stat--likes').textContent = array.likes;
+
+  // 2. Содержимое счетчика комментариев
+  photo.querySelector('.picture__stat--comments').textContent = array.comments.length;
+
+  // 3. Путь к фотографии
+  photo.querySelector('.picture__img').src = array.url;
+
+  return photo;
+};
+
+// Заполнение фрагмента
+for (var i = 0; i < data.length; i++) {
+  fragment.appendChild(createPhoto(data[i]));
+}
+
+// Вставка фрагмента на страницу
+photosList.appendChild(fragment);
+
+// ---
+// Работа с модалкой .big-picture
+// ---
+
+// Модалка большой фотографии
+var bigPhoto = document.querySelector('.big-picture');
+
+// Показ модалки
+bigPhoto.classList.remove('hidden');
+
+// Наполнение тега модалки контентом
+// 1. Путь к фотографии
+bigPhoto.querySelector('.big-picture__img img').src = data[0].url;
+
+// 2. Содержимое счетчика лайков
+bigPhoto.querySelector('.likes-count').textContent = data[0].likes;
+
+// 3. Содержимое счетчика комментариев
+bigPhoto.querySelector('.comments-count').textContent = data[0].comments.length;
+
+// 4. Содержимое блока комментариев
+// Список ul комментариев
+var commentsList = bigPhoto.querySelector('.social__comments');
+
+// Коллекция li комментариев
+var commentsCollection = commentsList.querySelectorAll('.social__comment');
+
+// Удаление лишней li если комментарий всего один
+if (data[0].comments.length < commentsCollection.length) {
+  commentsCollection[1].classList.add('visually-hidden');
+}
+
+// Заполнение атрибутов и свойств контентом
+for (i = 0; i < data[0].comments.length; i++) {
+  commentsCollection[i].querySelector('img').src = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
+  commentsCollection[i].querySelector('.social__text').textContent = data[0].comments[i];
+}
+
+// Заполнение описания фотографии
+bigPhoto.querySelector('.social__caption').textContent = data[0].description;
+
+// Скрытие блоков
+bigPhoto.querySelector('.social__comment-count').classList.add('visually-hidden');
+bigPhoto.querySelector('.social__loadmore').classList.add('visually-hidden');
